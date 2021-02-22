832b0728ded60384f63d64bac94291a442bcc19f
Santiago Hartmann <santiagohartmann97@gmail.com>
2021-02-22 19:33:23 +0100

    aga

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from mongoConnections import read, insert
 from bson import json_util, ObjectId
 from checking import check_mandatory, check_groups
 
-app = flask.Flask("lotr_quotes")
+app = Flask("lotr_quotes")
 app.config["DEBUG"] = True
 
 @app.route("/welcome")
@@ -23,6 +23,16 @@ def quotes_by_character(character):
     data = read(q, project={"char":1, "dialog":1, "_id":0})
     if len(data) == 0:
         return {"Error":"The character is still not in the Database"}
+    return json_util.dumps(data)
+
+@app.route("/quotesbymovie/<movie>")
+def quotes_by_movie(movie):
+
+    q = {"movie":movie}
+    if not check_groups(q,"movie",["The Fellowshi of the Ring", "The Two Towers", "The Return of the King"]):
+        return {"Error":"The movie is still not in the database"}
+    data = read(q, project={"name":1, "dialog":1, "_id":0})
+
     return json_util.dumps(data)
 
 app.run()
